3e7b5983207e327683a7a0145da470126eb5f0ed
Nikolai <SuvorovNM@gmail.com>
2021-12-04 16:40:31 +0300

    Added randomness to transition executing

--- DataPetriNet/DataPetriNet.cs
+++ DataPetriNet/DataPetriNet.cs
@@ -1,18 +1,28 @@
 ﻿using DataPetriNet.DPNElements;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DataPetriNet
 {
     public class DataPetriNet // TODO: Add Randomness
     {
+        private Random randomGenerator;
         public List<Place> Places { get; set; }
         public List<Transition> Transitions { get; set; }
         public VariablesStore Variables { get; set; }
 
+        public DataPetriNet()
+        {
+            randomGenerator = new Random();
+            Places = new List<Place>();
+            Transitions = new List<Transition>();
+        }
+
         public bool MakeStep()
         {
-            var canMakeStep = false;
-            foreach (var transition in Transitions)
+            var canMakeStep = false; // TODO: Find a more quicker way to get random elements?
+            foreach (var transition in Transitions.OrderBy(x => randomGenerator.Next()))
             {
                 canMakeStep = transition.TryFire(Variables);
                 if (canMakeStep)
