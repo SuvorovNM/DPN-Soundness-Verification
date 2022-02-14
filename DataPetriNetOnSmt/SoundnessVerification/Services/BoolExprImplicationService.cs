57854db00f048c42a8e3de81a01889ab7a503758
Nikolai <SuvorovNM@gmail.com>
2022-02-14 11:00:15 +0300

    Fix manual implication for gt, lt

--- DataPetriNetOnSmt/SoundnessVerification/Services/BoolExprImplicationService.cs
+++ DataPetriNetOnSmt/SoundnessVerification/Services/BoolExprImplicationService.cs
@@ -29,29 +29,85 @@ namespace DataPetriNetOnSmt.SoundnessVerification.Services
                 throw new ArgumentNullException(nameof(secondVar));
             }
 
-            var optimizer = SetOptimizer(concatenatedExpressionGroup, varToOverwrite);
-
-            optimizer.MkMinimize(varToOverwrite);
-            if (optimizer.Check() == Status.SATISFIABLE)
+            if (varToOverwrite.IsReal || secondVar.IsReal)
             {
-                var minVarValue = optimizer.Model.Consts
-                    .FirstOrDefault(x => x.Key.Name.ToString() == varToOverwrite.ToString())
-                    .Value;
+                var relaxedOptimizer = SetOptimizer(FormRelaxedExpressionList(concatenatedExpressionGroup), varToOverwrite);
+                relaxedOptimizer.MkMinimize(varToOverwrite);
 
-                if (minVarValue.IsRatNum && minVarValue.ToString() != realMin.ToString()
-                    || minVarValue.IsIntNum && minVarValue.ToString() != integerMin.ToString())
+                var hardOptimizer = SetOptimizer(concatenatedExpressionGroup, varToOverwrite);
+                hardOptimizer.MkMinimize(varToOverwrite);
+
+                if (relaxedOptimizer.Check() == Status.SATISFIABLE && hardOptimizer.Check() == Status.SATISFIABLE)
                 {
-                    return includeEquality
-                        ? ContextProvider.Context.MkGe((ArithExpr)secondVar, (ArithExpr)minVarValue)
-                        : ContextProvider.Context.MkGt((ArithExpr)secondVar, (ArithExpr)minVarValue);
-                }
+                    var relaxedMinVarValue = relaxedOptimizer.Model.Consts
+                        .FirstOrDefault(x => x.Key.Name.ToString() == varToOverwrite.ToString())
+                        .Value;
+                    var hardMinVarValue = hardOptimizer.Model.Consts
+                        .FirstOrDefault(x => x.Key.Name.ToString() == varToOverwrite.ToString())
+                        .Value;
 
-                // If no value restrictions return true
-                return ContextProvider.Context.MkTrue();
+                    var canBeEquality = relaxedMinVarValue.ToString() == hardMinVarValue.ToString();
+
+                    if (relaxedMinVarValue.IsRatNum && relaxedMinVarValue.ToString() != realMin.ToString()
+                    || relaxedMinVarValue.IsIntNum && relaxedMinVarValue.ToString() != integerMin.ToString())
+                    {
+                        return includeEquality && canBeEquality
+                            ? ContextProvider.Context.MkGe((ArithExpr)secondVar, (ArithExpr)relaxedMinVarValue)
+                            : ContextProvider.Context.MkGt((ArithExpr)secondVar, (ArithExpr)relaxedMinVarValue);
+                    }
+                    return ContextProvider.Context.MkTrue();
+                }
             }
+            else
+            {
+                var optimizer = SetOptimizer(concatenatedExpressionGroup, varToOverwrite);
+                optimizer.MkMinimize(varToOverwrite);
+
+                if (optimizer.Check() == Status.SATISFIABLE)
+                {
+                    var minVarValue = optimizer.Model.Consts
+                        .FirstOrDefault(x => x.Key.Name.ToString() == varToOverwrite.ToString())
+                        .Value;
+
+                    if (minVarValue.IsRatNum && minVarValue.ToString() != realMin.ToString()
+                        || minVarValue.IsIntNum && minVarValue.ToString() != integerMin.ToString())
+                    {
+                        return includeEquality
+                            ? ContextProvider.Context.MkGe((ArithExpr)secondVar, (ArithExpr)minVarValue)
+                            : ContextProvider.Context.MkGt((ArithExpr)secondVar, (ArithExpr)minVarValue);
+                    }
+
+                    // If no value restrictions return true
+                    return ContextProvider.Context.MkTrue();
+                }
+            }            
 
             // If expressionGroup is unsatisfiable, false is returned
             return ContextProvider.Context.MkFalse();
+        }
+
+        private static List<BoolExpr> FormRelaxedExpressionList(IEnumerable<BoolExpr> concatenatedExpressionGroup)
+        {
+            List<BoolExpr> expressionsToExamine = new List<BoolExpr>();
+            foreach (var expression in concatenatedExpressionGroup)
+            {
+                if (expression.IsEq || expression.IsGE || expression.IsLE)
+                {
+                    expressionsToExamine.Add(expression);
+                }
+                if (expression.IsGT)
+                {
+                    var relaxedExpression = ContextProvider.Context.MkGe((ArithExpr)expression.Args[0], (ArithExpr)expression.Args[1]);
+                    expressionsToExamine.Add(relaxedExpression);
+                }
+                if (expression.IsLT)
+                {
+                    var relaxedExpression = ContextProvider.Context.MkLe((ArithExpr)expression.Args[0], (ArithExpr)expression.Args[1]);
+                    expressionsToExamine.Add(relaxedExpression);
+                }
+            }
+
+            return expressionsToExamine;
         }
 
         public BoolExpr GetImplicationOfLessExpression(
@@ -73,24 +129,55 @@ namespace DataPetriNetOnSmt.SoundnessVerification.Services
                 throw new ArgumentNullException(nameof(secondVar));
             }
 
-            var optimizer = SetOptimizer(concatenatedExpressionGroup, varToOverwrite);
-
-            optimizer.MkMaximize(varToOverwrite);
-            if (optimizer.Check() == Status.SATISFIABLE)
+            if (varToOverwrite.IsReal || secondVar.IsReal)
             {
-                var maxVarValue = optimizer.Model.Consts
-                    .FirstOrDefault(x => x.Key.Name.ToString() == varToOverwrite.ToString())
-                    .Value;
+                var relaxedOptimizer = SetOptimizer(FormRelaxedExpressionList(concatenatedExpressionGroup), varToOverwrite);
+                relaxedOptimizer.MkMaximize(varToOverwrite);
 
-                if (maxVarValue.IsRatNum && maxVarValue.ToString() != realMax.ToString()
-                    || maxVarValue.IsIntNum && maxVarValue.ToString() != integerMax.ToString())
+                var hardOptimizer = SetOptimizer(concatenatedExpressionGroup, varToOverwrite);
+                hardOptimizer.MkMaximize(varToOverwrite);
+
+                if (relaxedOptimizer.Check() == Status.SATISFIABLE && hardOptimizer.Check() == Status.SATISFIABLE)
                 {
-                    return includeEquality
-                        ? ContextProvider.Context.MkLe((ArithExpr)secondVar, (ArithExpr)maxVarValue)
-                        : ContextProvider.Context.MkLt((ArithExpr)secondVar, (ArithExpr)maxVarValue);
+                    var relaxedMaxVarValue = relaxedOptimizer.Model.Consts
+                        .FirstOrDefault(x => x.Key.Name.ToString() == varToOverwrite.ToString())
+                        .Value;
+                    var hardMaxVarValue = hardOptimizer.Model.Consts
+                        .FirstOrDefault(x => x.Key.Name.ToString() == varToOverwrite.ToString())
+                        .Value;
+
+                    var canBeEquality = relaxedMaxVarValue.ToString() == hardMaxVarValue.ToString();
+
+                    if (relaxedMaxVarValue.IsRatNum && relaxedMaxVarValue.ToString() != realMax.ToString())
+                    {
+                        return includeEquality && canBeEquality
+                            ? ContextProvider.Context.MkLe((ArithExpr)secondVar, (ArithExpr)relaxedMaxVarValue)
+                            : ContextProvider.Context.MkLt((ArithExpr)secondVar, (ArithExpr)relaxedMaxVarValue);
+                    }
+                    return ContextProvider.Context.MkTrue();
                 }
-                // If no value restrictions return true
-                return ContextProvider.Context.MkTrue();
+            }
+            else
+            {
+                var optimizer = SetOptimizer(concatenatedExpressionGroup, varToOverwrite);
+                optimizer.MkMaximize(varToOverwrite);
+
+                if (optimizer.Check() == Status.SATISFIABLE)
+                {
+                    var maxVarValue = optimizer.Model.Consts
+                        .FirstOrDefault(x => x.Key.Name.ToString() == varToOverwrite.ToString())
+                        .Value;
+
+                    if (maxVarValue.IsIntNum && maxVarValue.ToString() != integerMax.ToString())
+                    {
+                        return includeEquality
+                            ? ContextProvider.Context.MkLe((ArithExpr)secondVar, (ArithExpr)maxVarValue)
+                            : ContextProvider.Context.MkLt((ArithExpr)secondVar, (ArithExpr)maxVarValue);
+                    }
+
+                    // If no value restrictions return true
+                    return ContextProvider.Context.MkTrue();
+                }
             }
 
             // If expressionGroup is unsatisfiable, false is returned
