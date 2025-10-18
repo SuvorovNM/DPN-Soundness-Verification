fe40d72b525eb70df6094629c53be21145bc6da3
nm.suvorov <nm.suvorov@skbkontur.ru>
2025-10-18 15:59:26 +0500

    StateSpace step 1

--- DPN.VerificationApp/StateSpace.xaml.cs
+++ DPN.VerificationApp/StateSpace.xaml.cs
@@ -102,6 +102,43 @@ namespace DPN.VerificationApp
 			ShowGraph(false);
 		}
 
+		private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = WindowState.Minimized;
+		}
+
+		private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (this.WindowState == WindowState.Maximized)
+			{
+				this.WindowState = WindowState.Normal;
+			}
+			else
+			{
+				this.WindowState = WindowState.Maximized;
+			}
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void TitleBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+			{
+				if (e.ClickCount == 2)
+				{
+					MaximizeButton_Click(sender, e);
+				}
+				else
+				{
+					this.DragMove();
+				}
+			}
+		}
+
 		public bool IsOverlayVisible => GraphTooLargeOverlay.Visibility == Visibility.Visible;
 	}
 }
